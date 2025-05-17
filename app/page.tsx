52c6bafbdf89e6504c64d14e36eef90c0bc48684
Brian Valette <47712216+brian-xu-vlt@users.noreply.github.com>
2025-05-17 08:19:48 +0200

    pnpm dlx shadcn@latest init

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
 
     return (
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            Hi
+            ✅Hi
         </main>
     )
 }
