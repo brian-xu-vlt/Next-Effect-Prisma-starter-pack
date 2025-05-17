b1591ba97637191568eb7a67e12c3b8b3c7a0e32
Brian Valette <47712216+brian-xu-vlt@users.noreply.github.com>
2025-05-17 10:16:37 +0200

    misc - biome & config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
+  // watchOptions: {
+  //   pollIntervalMs: 1000,
+  // }
 };
 
 export default nextConfig;
