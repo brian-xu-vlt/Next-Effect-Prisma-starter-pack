b382dc150b3f29a9e871738a97ef2af5c2d73a40
Brian Valette <47712216+brian-xu-vlt@users.noreply.github.com>
2025-05-17 17:58:31 +0200

    pnpm dlx shadcn@latest init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from 'auth';
 import { SignIn } from 'components/SignIn';
+import { Button } from 'modules/components/ui/button';
 
 export default async function Home() {
   const session = await auth();
@@ -8,6 +9,7 @@ export default async function Home() {
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
