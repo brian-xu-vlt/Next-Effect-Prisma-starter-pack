759eb95fb7cd68b1d01d43f235ba41aa5a18ca9d
Brian Valette <47712216+brian-xu-vlt@users.noreply.github.com>
2025-06-18 21:46:45 +0200

    add context

--- app/dashboard/example-combobox/page.tsx
+++ app/dashboard/example-combobox/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-
+import { SiteHeader } from 'modules/components/site-header';
+import { SidebarInset, SidebarProvider } from 'modules/components/ui/sidebar';
 import * as React from 'react';
 import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react';
 
@@ -18,6 +19,13 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from 'modules/components/ui/popover';
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardAction,
+} from 'modules/components/ui/card';
 
 const frameworks = [
   {
@@ -43,6 +51,42 @@ const frameworks = [
 ];
 
 export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+                <Card className="@container/card">
+                  <CardHeader>
+                    <CardDescription>Total Revenue</CardDescription>
+                    <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                      Title
+                    </CardTitle>
+                    <CardAction>
+                      <Combobox />
+                    </CardAction>
+                  </CardHeader>
+                </Card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
+
+function Combobox() {
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState('');
 
